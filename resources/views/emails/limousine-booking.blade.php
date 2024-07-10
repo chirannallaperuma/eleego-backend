83a9a00a27ffdab91bdb39d03e091e45c4cd896d
chiran nallaperuma <chiran@intaap.com>
2024-07-10 15:55:58 +0530

    fixed email template issues

--- resources/views/emails/limousine-booking.blade.php
+++ resources/views/emails/limousine-booking.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>New Limousine Quotation Received</title>
+    <title>New Quotation Received</title>
     <style>
         body {
             font-family: 'Arial', sans-serif;
@@ -83,12 +83,12 @@
 <body>
     <div class="container">
         <div class="header">
-            <h1>Limousine Quotation</h1>
+            <h1>{{ $quotation->category == 'limousine' ? 'Limousine' : 'Rental' }} Quotation</h1>
         </div>
         <div class="content">
             <p>Hello,</p>
             
-            <p>We are pleased to inform you that a new limousine quotation has been received. Below are the details of the quotation:</p>
+            <p>We are pleased to inform you that a new quotation has been received. Below are the details of the quotation:</p>
             
             <div class="quotation-details">
                 <p><strong>Quotation ID:</strong> {{ $quotation->id }}</p>
